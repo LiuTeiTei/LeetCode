782d620ce72a8975e9a26b9799944f23169afeee
LiuTeiTei <ltt_catchyou@163.com>
2021-11-04 21:08:10 +0800

    Medium: 3

---
+++ Medium/3_LongestSubstringWithoutRepeatingCharacters.ts
@@ -0,0 +1,97 @@
+/* 
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+给定一个字符串 s ，请你找出其中不含有重复字符的最长子串的长度。
+
+示例 1:
+输入: s = "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+
+示例 2:
+输入: s = "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+
+示例 3:
+输入: s = "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     
+示例 4:
+输入: s = ""
+输出: 0
+ 
+提示：
+
+0 <= s.length <= 5 * 104
+s 由英文字母、数字、符号和空格组成
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+// 滑动窗口
+// 左右两个指针，分别指向 0、1 元素，右指针不断右移直到发现重复的字符，此时左指针右移一个并且右指针重置到左指针的右边一个。
+// O(n^3)   Runtime:488 ms   Memory:46 MB
+{
+  function lengthOfLongestSubstring(s: string): number {
+    if (s.length < 2) return s.length;
+  
+    let max = 1;
+    let arr: string[];
+  
+    for (let i = 0; i < s.length; i++) {
+      arr = [s[i]];
+      for (let j = i + 1; j < s.length; j++) {
+        const isRepeated = arr.indexOf(s[j]) > -1;
+  
+        if (isRepeated) {
+          arr = [];
+          break
+        } else {
+          arr.push(s[j]);
+          max = Math.max(max, arr.length);
+        }
+      }
+    }
+  
+    return max;
+  };
+}
+
+// 滑动窗口
+// 查找是否相同优化
+// O(n^2)   Runtime:104 ms   Memory:44 MB
+{
+  function lengthOfLongestSubstring(s: string): number {
+    // 哈希集合，记录每个字符是否出现过
+    const stringSet = new Set();
+    // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+    let rightP = -1;
+    let max = 0;
+    let n = s.length;
+
+    for (let i = 0; i < n; i++) {
+      if (i !== 0) {
+        // 左指针向右移动一格，移除一个字符
+        stringSet.delete(s.charAt(i - 1));
+      }
+
+      while (rightP + 1 < n && !stringSet.has(s.charAt(rightP + 1))) {
+        // 不断地移动右指针
+        stringSet.add(s.charAt(rightP + 1));
+        ++rightP;
+      }
+
+      // 第 i 到 rk 个字符是一个极长的无重复字符子串
+      max = Math.max(max, rightP - i + 1);
+    }
+  
+    return max;
+  };
+}
+
+console.log(lengthOfLongestSubstring('asbcafkdj'));
