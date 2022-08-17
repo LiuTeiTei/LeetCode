72f594a865643186da94438c00ff312a3077b0d6
LiuTeiTei <ltt_catchyou@163.com>
2022-08-18 00:11:38 +0800

    Medium: 45

---
+++ Medium/45_jump-game-ii.ts
@@ -0,0 +1,49 @@
+/* 
+https://leetcode.com/problems/jump-game-ii/
+https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0045.%E8%B7%B3%E8%B7%83%E6%B8%B8%E6%88%8FII.md
+
+给你一个非负整数数组 nums ，你最初位于数组的第一个位置。
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+假设你总是可以到达数组的最后一个位置。
+
+示例 1:
+输入: nums = [2,3,1,1,4]
+输出: 2
+解释: 跳到最后一个位置的最小跳跃数是 2。
+     从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+
+示例 2:
+输入: nums = [2,3,0,1,4]
+输出: 2
+ 
+
+提示:
+1 <= nums.length <= 104
+0 <= nums[i] <= 1000
+*/
+
+
+// 局部最优：当前可移动距离尽可能多走，如果还没到终点，步数再加一。
+// 整体最优：一步尽可能多走，从而达到最小步数
+function jump(nums: number[]): number {
+  let result = 0
+  // 当前覆盖的最远距离下标
+  let curDistance = 0
+  // 下一步覆盖的最远距离下标
+  let nextDistance = 0
+
+  // 注意终止条件是倒数第二个元素
+  for (let i = 0; i < nums.length - 1; i++) {
+    nextDistance = Math.max(nums[i] + i, nextDistance)
+    // 走完当前覆盖的最远距离下标，再往前走一步
+    if (i === curDistance) {
+      curDistance = nextDistance
+      result++
+    }
+  }
+
+  return result
+};
+
+jump([2,3,1,1,4])
--- README.md
+++ README.md
@@ -477,3 +477,4 @@ void backtracking(参数) {
 - [53. Maximum Subarray](https://github.com/LiuTeiTei/LeetCode/blob/master/Medium/53_maximum-subarray.ts)
 - [122. Best Time to Buy and Sell Stock II](https://github.com/LiuTeiTei/LeetCode/blob/master/Medium/122_best-time-to-buy-and-sell-stock-ii.ts)
 - [55. Jump Game](https://github.com/LiuTeiTei/LeetCode/blob/master/Medium/55_jump-game.ts)
+- [45. Jump Game II](https://github.com/LiuTeiTei/LeetCode/blob/master/Medium/45_jump-game-ii.ts)
