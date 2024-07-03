e7dbe2e35951a9f3eb735f068f2f23695dd9997c
LiuTeiTei <ltt_catchyou@163.com>
2024-07-03 21:55:40 +0800

    Easy: 104

---
+++ Easy/104_maximum-depth-of-binary-tree.ts.ts
@@ -0,0 +1,45 @@
+/* 
+https://leetcode.com/problems/maximum-depth-of-binary-tree/
+https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0104.%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E6%9C%80%E5%A4%A7%E6%B7%B1%E5%BA%A6.md
+
+给定一个二叉树，找出其最大深度。
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+说明: 叶子节点是指没有子节点的节点。
+
+示例 1：
+给定二叉树 [3,9,20,null,null,15,7]，
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+
+示例 2：
+输入：root = [1,null,2]
+输出：2
+
+提示：
+树中节点的数量在 [0, 104] 区间内。
+-100 <= Node.val <= 100
+*/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// 递归
+function maxDepth(root: TreeNode | null): number {
+  if (!root) return 0
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+};
