db6313156bf5e8432f459e8051980efa8efbd520
LiuTeiTei <ltt_catchyou@163.com>
2024-07-16 22:01:52 +0800

    Easy: 121

--- Easy/121_best-time-to-buy-and-sell-stock.ts
+++ Easy/121_best-time-to-buy-and-sell-stock.ts
@@ -26,45 +26,41 @@ https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0121.%E4
 0 <= prices[i] <= 104
 */
 
-// 贪心算法：取最左最小值，取最右最大值，得到的差值就是最大利润
+// 贪心算法
+// 取最左最小值，取最右最大值，得到的差值就是最大利润
 // 时间复杂度：O(n)，空间复杂度：O(1)
-{
-  function maxProfit(prices: number[]): number {
-    let min = Number.MAX_SAFE_INTEGER
-    let result = 0
-  
-    for (let i = 0; i < prices.length; i++) {
+function maxProfit(prices: number[]): number {
+  let min = prices[0]
+  let result = 0
+
+  for (let i = 1; i < prices.length; i++) {
       min = Math.min(min, prices[i])
       result = Math.max(result, prices[i] - min)
-    }
-  
-    return result
-  };
-}
-
-// 动态规划
-// 时间复杂度：O(n)，空间复杂度：O(n)
-function maxProfit(prices: number[]): number {
-  // dp[i][0] 表示第i天持有股票所得最多现金，dp[i][1] 表示第i天不持有股票所得最多现金
-  const dp: number[][] = []
-
-  // 初始化
-  dp[0] = [-prices[0], 0]
-
-  // 从前往后遍历
-  for (let i = 1; i < prices.length; i++) {
-    dp[i] = []
-
-    // 第 i 天持有股票即 dp[i][0]，那么可以由两个状态推出来:
-    // 第 i-1 天就持有股票，那么就保持现状，所得现金就是昨天持有股票的所得现金，即 dp[i - 1][0]
-    // 第 i 天买入股票，所得现金就是买入今天的股票后所得现金，即 -prices[i]
-    dp[i][0] = Math.max(dp[i - 1][0], -prices[i])
-
-    // 第 i 天不持有股票即 dp[i][1]，那么可以由两个状态推出来:
-    // 第 i-1 天就不持有股票，那么就保持现状，所得现金就是昨天不持有股票的所得现金，即 dp[i - 1][1]
-    // 第 i 天卖出股票，所得现金就是按照今天股票价格卖出后所得现金，即 prices[i] + dp[i - 1][0]
-    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i])
   }
 
-  return dp[prices.length - 1][1]
+  return result
+};
+
+// 动态规划
+// 时间复杂度: O(n) 空间复杂度: O(2n)
+function maxProfit(prices: number[]): number {
+  // dp[i][0] 表示第i天不持有股票所得最多现金，dp[i][1] 表示第i天持有股票所得最多现金
+  const dp: number[][] = []
+  dp[0] = [0, -prices[0]]
+
+  for (let i = 1; i < prices.length; i++) {
+      dp[i] = []
+
+      // 第 i 天不持有股票即 dp[i][0]，那么可以由两个状态推出来:
+      // 第 i-1 天就不持有股票，那么就保持现状，所得现金就是昨天不持有股票的所得现金，即 dp[i - 1][0]
+      // 第 i 天卖出股票，所得现金就是按照今天股票价格卖出后所得现金，即 prices[i] + dp[i - 1][1]
+      dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+
+      // 第 i 天持有股票即 dp[i][1]，那么可以由两个状态推出来:
+      // 第 i-1 天就持有股票，那么就保持现状，所得现金就是昨天持有股票的所得现金，即 dp[i - 1][1]
+      // 第 i 天买入股票，所得现金就是买入今天的股票后所得现金，即 -prices[i]
+      dp[i][1] = Math.max(dp[i - 1][1], -prices[i])
+  }
+
+  return dp[prices.length - 1][0]
 };
